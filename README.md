2caa2f1d7795d399a9c5594f3f0370a062be6679
John Nyokabi <jwnyokabi@gmail.com>
2023-09-07 00:05:25 +0300

    file manager

---
+++ README.md
@@ -0,0 +1,24 @@
+# 0x04. Files manager
+
+## Requirements
+	* Ubuntu
+	* vim/vi/emacs/vscode
+	* node js
+	* Mocha
+	* redis
+	* mongoDB
+	* Kue
+	* Bull
+	* Image-Thumbnail
+
+## Tasks
+   * [ 0. Redis utils ]
+   * [ 1. MongoDB utils ]
+   * [ 2. First API ]
+   * [ 3. Create a new user ]
+   * [ 4. Authenticate a user ]
+   * [ 5. First file ]
+   * [ 6. Get and list file ]
+   * [ 7. File publish/unpublish ]
+   * [ 8. File data ]
+   * [ 9. Image Thumbnails ]
---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => console.log(error));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
